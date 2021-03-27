fb86dae07ef4d03a20b7a6ff6469243a9323d2d6
low2by <lemmanuel14@yahoo.com>
2021-03-26 19:15:10 -0600

    first commit

---
+++ facetracker.py
@@ -0,0 +1,67 @@
+#https://towardsdatascience.com/face-landmark-detection-with-cnns-tensorflow-cf4d191d2f0
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
+
+x_train = np.load( "face_landmarks_cleaned/x_train.npy" ) / 255
+y_train = np.load( "face_landmarks_cleaned/y_train.npy" ) / 96
+x_test = np.load( "face_landmarks_cleaned/x_test.npy" ) / 255
+y_test = np.load( "face_landmarks_cleaned/y_test.npy" ) / 96
+
+y_train = np.reshape( y_train , ( -1 , 1 , 1 , 30 ))
+y_test = np.reshape( y_test , ( -1 , 1 , 1 , 30 ))
+
+model_layers = [
+                
+    tf.keras.layers.Conv2D( 256 , input_shape=( 96 , 96 , 1 ) , kernel_size=( 3 , 3 ) , strides=2 , activation='relu' ),
+    tf.keras.layers.Conv2D( 256 , kernel_size=( 3 , 3 ) , strides=2 , activation='relu' ),
+    tf.keras.layers.BatchNormalization(),
+
+    tf.keras.layers.Conv2D( 128 , kernel_size=( 3 , 3 ) , strides=1 , activation='relu' ),
+    tf.keras.layers.Conv2D( 128 , kernel_size=( 3 , 3 ) , strides=1 , activation='relu' ),
+    tf.keras.layers.BatchNormalization(),
+
+    tf.keras.layers.Conv2D( 128 , kernel_size=( 3 , 3 ) , strides=1 , activation='relu' ),
+    tf.keras.layers.Conv2D( 128 , kernel_size=( 3 , 3 ) , strides=1 , activation='relu' ),
+    tf.keras.layers.BatchNormalization(),
+
+    tf.keras.layers.Conv2D( 64 , kernel_size=( 3 , 3 ) , strides=1 , activation='relu' ),
+    tf.keras.layers.Conv2D( 64 , kernel_size=( 3 , 3 ) , strides=1 , activation='relu' ),
+    tf.keras.layers.BatchNormalization(),
+
+    tf.keras.layers.Conv2D( 32 , kernel_size=( 3 , 3 ) , strides=1 , activation='relu' ),
+    tf.keras.layers.Conv2D( 32 , kernel_size=( 3 , 3 ) , strides=1 , activation='relu' ),
+    tf.keras.layers.BatchNormalization(),
+
+    tf.keras.layers.Conv2D( 30 , kernel_size=( 3 , 3 ) , strides=1 , activation='relu' ),
+    tf.keras.layers.Conv2D( 30 , kernel_size=( 3 , 3 ) , strides=1 , activation='relu' ),
+    tf.keras.layers.Conv2D( 30 , kernel_size=( 3 , 3 ) , strides=1 ),
+
+]
+model = tf.keras.Sequential( model_layers )
+model.compile( loss=tf.keras.losses.mean_squared_error , optimizer=tf.keras.optimizers.Adam( lr=0.0001 ) , metrics=[ 'mse' ] )
+
+model.fit(
+    x=x_train,
+    y=y_train,
+    epochs=10,
+    batch_size=50,
+
+)
+
+fig = plt.figure(figsize=( 50 , 50 ))
+
+for i in range( 1 , 6 ):
+    sample_image = np.reshape( x_test[i] * 255  , ( 96 , 96 ) ).astype( np.uint8 )
+    pred = model.predict( x_test[ i : i +1  ] ) * 96
+    pred = pred.astype( np.int32 )
+    pred = np.reshape( pred[0 , 0 , 0 ] , ( 15 , 2 ) )
+    fig.add_subplot( 1 , 10 , i )
+    plt.imshow( sample_image.T , cmap='gray' )
+    plt.scatter( pred[ : , 0 ] , pred[ : , 1 ] , c='yellow' )
+    
+plt.show()
+
